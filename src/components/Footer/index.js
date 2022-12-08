2e462b65af38a372d10b83dc6787e1869886068b
Guilherme Costa <guicm007@gmail.com>
2022-12-08 18:29:10 -0300

    feat: add footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+// import './style.css'
+
+const Footer = () => {
+  return (
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Rick and Morty Project 2022</p>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
