f6a6670a1c9d643f99c7f3cbb9c23c446c2ee9b0
Guilherme Costa <guicm007@gmail.com>
2022-12-08 18:47:17 -0300

    feat: import and use the components in home

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,18 @@
 import Head from 'next/head'
+import Footer from '../src/components/Footer'
+import Navbar from '../src/components/Navbar'
+import Header from '../src/components/Header'
 
 export default function Home() {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charset="utf-8" />
       </Head>
-      <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-              <a class="nav-link" href="#">Features</a>
-              <a class="nav-link" href="#">Pricing</a>
-              <a class="nav-link disabled">Disabled</a>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Navbar/>
+      <Header/>
+      <Footer/>
     </>
   )
 }
