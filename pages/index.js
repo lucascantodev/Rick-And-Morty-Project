8b03413ea0b95855bd4703322521e9435489fb86
Guilherme Costa <guicm007@gmail.com>
2022-12-10 02:44:21 -0300

    feat: consume api and list the responses

--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,9 @@ export default function Home() {
       <Navbar/>
       <Header/>
       <main>
-        <section class="py-5">
-          <div class="container px-4 px-lg-5 mt-5">
-            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+        <section className="py-5">
+          <div className="container px-4 px-lg-5 mt-5">
+            <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
               <CharactersList />
             </div>
           </div>
--- src/components/CharactersList/index.js
+++ src/components/CharactersList/index.js
@@ -1,25 +1,42 @@
+import { useFetch } from '../../utils/hook'
+import Spinner from '../Spinner'
+import Error from '../Error'
 
 const CharactersList = () => {
-  return (
-    <div class="col mb-5">
-        <div class="card h-100">
-            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-            <div class="card-body p-4">
-                <div class="text-center">
-                    <h5 class="fw-bolder">Fancy Product</h5>
-                    $40.00 - $80.00
+    const { data, isError, isLoading } = useFetch("character");
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
+    if (isError) {
+        return <Error />
+    }
+
+    return (
+        <>
+        {data.results.map((character) => (
+            <div className="col mb-5" key={character.id}>
+                <div className="card h-100">
+                    <img className="card-img-top" src={character.image} alt={character.name} />
+                    <div className="card-body p-4">
+                        <div className="text-center">
+                            <h5 className="fw-bolder">{character.name}</h5>
+                            {character.status} - {character.species}
+                        </div>
+                    </div>
+                    <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        <div className="text-center">
+                            <a className="btn btn-outline-dark mt-auto" href="#">
+                                View options
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                <div class="text-center">
-                    <a class="btn btn-outline-dark mt-auto" href="#">
-                        View options
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-  );
+        ))}
+        </>
+    );
 };
 
 export default CharactersList;
