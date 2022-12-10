e561206c646656f598b4ef69d3259e66f4fee102
Guilherme Costa <guicm007@gmail.com>
2022-12-10 02:39:36 -0300

    feat: add spinner and error components

---
+++ src/components/Error/index.js
@@ -0,0 +1,10 @@
+
+const Error = () => {
+    return (
+        <div class="alert alert-danger" role="alert">
+            An error has occurred!
+        </div>
+    );
+};
+
+export default Error;
