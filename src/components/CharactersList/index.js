c2a29a1c0062fc248ff8fc51ce1393ef8a6169b6
Guilherme Costa <guicm007@gmail.com>
2022-12-10 14:05:21 -0300

    feat: create card component

--- src/components/CharactersList/index.js
+++ src/components/CharactersList/index.js
@@ -1,4 +1,5 @@
 import { useFetch } from '../../utils/hook'
+import CharacterCard from '../CardComponent'
 import Spinner from '../Spinner'
 import Error from '../Error'
 
@@ -18,24 +19,13 @@ const CharactersList = () => {
             <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 <>
                 {data.results.map((character) => (
-                    <div className="col mb-5" key={character.id}>
-                        <div className="card h-100">
-                            <img className="card-img-top" src={character.image} alt={character.name} />
-                            <div className="card-body p-4">
-                                <div className="text-center">
-                                    <h5 className="fw-bolder">{character.name}</h5>
-                                    {character.status} - {character.species}
-                                </div>
-                            </div>
-                            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center">
-                                    <a className="btn btn-outline-dark mt-auto" href="#">
-                                        View options
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <CharacterCard
+                        image={character.image}
+                        name={character.name}
+                        status={character.status}
+                        species={character.species}
+                        key={character.id}
+                    />
                 ))}
                 </>
             </div>
