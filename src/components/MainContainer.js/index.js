cfdbfd068347b5198c830bba9f075db4151df3db
Guilherme Costa <guicm007@gmail.com>
2022-12-10 18:22:34 -0300

    feat: create main container componet

---
+++ src/components/MainContainer.js/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Navbar from '../Navbar'
+import Footer from '../Footer'
+
+const MainContainer = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charset="utf-8" />
+        <title>Rick and Morty Project - PWEB</title>
+      </Head>
+      <Navbar />
+        {children}
+      <Footer />
+    </>
+  );
+};
+
+export default MainContainer;
